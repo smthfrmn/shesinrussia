f8e54bde9ebb35f50533945a78b5fc761a68e0e0
Grace Poetzinger <gracepoetzinger@gmail.com>
2017-09-17 18:26:28 -0400

    init commit

---
+++ src/scripts/app.js
@@ -0,0 +1,48 @@
+var move = 0;
+var width = $('main').width() - ($('section').outerWidth() * 2);
+var speed = .5;
+
+$('html').on('mousewheel', function (e) {
+
+    var delta = e.originalEvent.wheelDelta;
+    var scrollTo = (e.originalEvent.wheelDelta * -1);
+    if (delta < 0) {
+       move += speed * scrollTo;
+       if (move > width) {
+         move = width;
+       }
+    } else if (delta > 0) {
+       move -= speed * delta;
+       if (move < 0) {
+         move = 0;
+       }
+    }
+    document.documentElement.style.overflow = 'hidden';
+    $( "main" ).css( "transform", "translate3d(-" + move + "px, 0px, 0px)", "width", width );
+});
+
+function updateCircle() {
+  //Manages updating the circle's text and size.
+  var circleArray = $(".circle");
+  var circleSvgArray = $(".circle-svg");
+  var circleTextArray = $(".circle-text");
+  console.log(circleArray.length)
+  for (i = 0; i < circleArray.length; i++) {
+    sizeCircleToText( circleArray[i], circleSvgArray[i], circleTextArray[i], .705 );
+  };
+}
+
+function sizeCircleToText(pathObject,targetSVG,targetText,charWidth){
+//Calcs the circumference of the circle needed and applies it.
+  r = (targetText.innerHTML.length*charWidth); //Radius
+  //r = (targetText.getComputedTextLength()+1)/6; //Maybe?
+  d = (r*2); //diameter
+  path = "M"+ (d) +","+ (d) + "m "+ (-r) +",0 a "+ (r) +","+ (r) +" 0 1,1 "+ (d) +",0 a "+r+","+r+" 0 1,1 "+ (-d) +",0"; //This gibberish is the path construction for a circle. Better way to do this or concat all this?
+  viewBox = "0 0 "+d*2+" "+d*2; //The viewbox rect
+  console.log(r, d, path, viewBox);
+  console.log(targetText.innerHTML);
+  pathObject.setAttribute("d",path);
+  targetSVG.setAttribute("viewBox",viewBox);
+}
+
+updateCircle();
