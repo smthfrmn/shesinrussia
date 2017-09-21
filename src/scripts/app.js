67fb1a4bacc8d64ae3a1c52984a0fe8002c5225b
Grace Poetzinger <gracepoetzinger@gmail.com>
2017-09-21 16:35:08 -0400

    fixed firefox bugs, fixed font error, removed console logs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -8,20 +8,33 @@ setTimeout(function(){
   if ((screen.width < 1025) || (screen.height < 480)) {
     $('body').addClass('horizontal');
   } else {
-    $('html').on('mousewheel', function (e) {
-
-        var delta = e.originalEvent.wheelDelta;
-        var scrollTo = (e.originalEvent.wheelDelta * -1);
-        if (delta < 0) {
-           move += speed * scrollTo;
-           if (move > width) {
-             move = width;
-           }
-        } else if (delta > 0) {
-           move -= speed * delta;
-           if (move < 0) {
-             move = 0;
-           }
+    $('html').on('wheel', function (e) {
+        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+          var delta = e.originalEvent.deltaY;
+          if (delta < 0) {
+             move -= speed * -delta;
+             if (move < 0) {
+               move = 0;
+             }
+          } else if (delta > 0) {
+             move += speed * delta;
+             if (move > width) {
+               move = width;
+             }
+          }
+        } else {
+          var delta = e.originalEvent.wheelDelta;
+          if (delta < 0) {
+             move += speed * -delta;
+             if (move > width) {
+               move = width;
+             }
+          } else if (delta > 0) {
+             move -= speed * delta;
+             if (move < 0) {
+               move = 0;
+             }
+          }
         }
         document.documentElement.style.overflow = 'hidden';
         $( "main" ).css( "transform", "translate3d(-" + move + "px, 0px, 0px)", "width", width );
@@ -34,7 +47,6 @@ function updateCircle() {
   var circleArray = $(".circle");
   var circleSvgArray = $(".circle-svg");
   var circleTextArray = $(".circle-text");
-  console.log(circleArray.length)
   for (i = 0; i < circleArray.length; i++) {
     sizeCircleToText( circleArray[i], circleSvgArray[i], circleTextArray[i], .705 );
   };
@@ -47,8 +59,6 @@ function sizeCircleToText(pathObject,targetSVG,targetText,charWidth){
   d = (r*2); //diameter
   path = "M"+ (d) +","+ (d) + "m "+ (-r) +",0 a "+ (r) +","+ (r) +" 0 1,1 "+ (d) +",0 a "+r+","+r+" 0 1,1 "+ (-d) +",0"; //This gibberish is the path construction for a circle. Better way to do this or concat all this?
   viewBox = "0 0 "+d*2+" "+d*2; //The viewbox rect
-  console.log(r, d, path, viewBox);
-  console.log(targetText.innerHTML);
   pathObject.setAttribute("d",path);
   targetSVG.setAttribute("viewBox",viewBox);
 }
